12efdc5b902d9af37656b83348127e7b173887b3
Shubham Bandagale <sbandagale18@gmail.com>
2021-11-12 20:56:01 +0530

    ADDED:VALIDATION to fields in the REST call

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDto.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDto.java
@@ -2,11 +2,15 @@ package com.bridgelabz.employeepayrollapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class EmployeePayrollDto {
 
-
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "INVALID EMPLOYEE NAME !!!")
     public String empName;
+    @Min(value = 500, message = "MINIMUM WAGE SHOULD BE MORE THAN 500")
     public long salary;
 
     public EmployeePayrollDto(String empName, long salary) {
