fc54f06d90bebd3cf415e149ea96bb353812c83b
Shubham Bandagale <sbandagale18@gmail.com>
2021-11-12 15:00:01 +0530

    ADDED:Loggin is activated using @SlF4J

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,12 +1,15 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeePayrollAppApplication {
+@Slf4j
+public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        SpringApplication.run(EmployeePayrollApplication.class, args);
+         log.info("EMPLOYEE PAYROLL APP STARTED !!! ");
     }
 }
