ae729b24ffa2c54ee264ee081d09fa2066c7f8b2
Shubham Bandagale <sbandagale18@gmail.com>
2021-11-13 00:59:39 +0530

    ADDE: Validations and exception handlers for new properties

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDto.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDto.java
@@ -3,8 +3,7 @@ package com.bridgelabz.employeepayrollapp.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,14 +17,20 @@ class EmployeePayrollDto {
     @Min(value = 500, message = "MINIMUM WAGE SHOULD BE MORE THAN 500")
     public long salary;
 
+    @Pattern(regexp = "male|female|third", message = "INVALID ENTRY FOR GENDER !!! ")
     public String gender;
 
     @JsonFormat(pattern ="dd MMM yyyy")
+    @NotNull(message = "DATE CANNOT BE EMPTY !!!")
+    @PastOrPresent(message = "startDate should be past or today's date !!! ")
     public LocalDate startDate;
 
+    @NotBlank(message = "NOTE CANNOT BE BLANK !!!")
     public String note;
 
+    @NotNull(message = "DEPARTMENT SHOULD NOT BE EMPTY !!!")
     public List<String> departments;
-    
+
+    @NotBlank(message ="PLEASE ADD A PROFILE PICTURE !!!" )
     public String profilePic;
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionalHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionalHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayrollapp.exceptions;
 
 import com.bridgelabz.employeepayrollapp.dto.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,21 +14,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionalHandler {
+    private static final String message = "EXCEPTION WHILE PROCESSING REST REQUEST :-( ";
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDto> handleHTTPMessageNotReadableException( HttpMessageNotReadableException exception) {
+        log.error("INVALID DATE FORMAT !!" , exception);
+        ResponseDto responseDto = new ResponseDto(message, "Should have date in this format -> dd MMM yyyy");
+        return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.BAD_REQUEST);
+    }
+
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-        List<String> errMsg = errorList.stream()
-                .map(objErr -> objErr.getDefaultMessage())
-                .collect(Collectors.toList());
-        ResponseDto responseDto = new ResponseDto("Exception while processing REST Request", errMsg);
-        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException (MethodArgumentNotValidException exception){
+            List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+            List<String> errMsg = errorList.stream()
+                    .map(objErr -> objErr.getDefaultMessage())
+                    .collect(Collectors.toList());
+            ResponseDto responseDto = new ResponseDto(message, errMsg);
+            return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmployeePayrollException.class)
-    public ResponseEntity<ResponseDto> handleEmployeePayrollException(EmployeePayrollException exception) {
-        ResponseDto responseDto = new ResponseDto("EXCEPTION WHILE PROCESSING REST REQUEST",
-                exception.getMessage());
-        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ResponseDto> handleEmployeePayrollException (EmployeePayrollException exception){
+            ResponseDto responseDto = new ResponseDto(message, exception.getMessage());
+            return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
     }
+
 }
